1081999ed57b875eed15cd6c8e75a66af843b5da
Jennyfer17 <tholejennyfer@gmail.com>
2024-02-03 16:31:52 +0200

    saving changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react'
+import Die from './Die';
+import {nanoid} from 'nanoid'
 
 function App() {
+
+  const [dice, setDice] = useState(allNewDice())
+
+  const diceElements = dice.map((die)=>
+  {  
+    return <Die 
+    key={die.id} 
+    value={die.value}
+    isHeld={die.isHeld}
+    holdDice={()=>holdDice(die.id)}
+    />
+  })
+
+  function allNewDice() {
+    const newArray = []
+    for (let i=0; i<10; i++) {
+      const randomNumber = Math.ceil(Math.random()*6)
+      newArray.push({
+        value: randomNumber,
+        isHeld: false,
+        id: nanoid()
+      })
+    }
+
+    return newArray
+  }
+
+  function rollDice() {
+    setDice(allNewDice())
+  }
+
+  function holdDice(id) {
+    setDice(prevDice=>{
+      prevDice.map((die)=>{
+        return die.id === id ? {...die, isHeld: !die.isHeld} : die
+      })
+    })
+    // dice.map(
+    //   (die)=>{
+    //     return die.id === id ? {...die, isHeld: !die.isHeld} : die
+    //   }
+    // )
+  }
+  console.log(dice)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <div className="container">
+       {diceElements}
+      </div>
+      <button onClick={rollDice}>
+        <h2>Roll</h2>
+      </button>
+    </main>
+  )
 }
 
 export default App;
---
+++ src/Die.js
@@ -0,0 +1,12 @@
+
+export default function Die(props) {
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "white"
+      }
+    return (
+        <div style={styles} className="die" onClick={props.holdDice}>
+            <h2>{props.value}</h2>
+        </div>
+        
+    )
+}
